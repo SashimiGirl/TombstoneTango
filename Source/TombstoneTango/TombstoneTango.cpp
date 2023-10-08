afba59228fb941c94cb6da8057b00cebc0a78a6e
Sara Maemoto <saramaemoto@gmail.com>
2023-10-07 17:45:58 -0700

    Initial commit

---
+++ Source/TombstoneTango/TombstoneTango.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TombstoneTango.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TombstoneTango, "TombstoneTango" );
